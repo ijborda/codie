f767699aa5b3a1190c6043c6028eaf5f0ef9297e
Imari Joy Borda <imari.borda2018@gmail.com>
2022-04-08 23:31:22 +0800

    Refactor

--- js/main.js
+++ js/main.js
@@ -2,64 +2,58 @@
 "use strict";
 
 // Set listener
-document.querySelector('#submit').addEventListener('click', displayData)
+document.querySelector('#submit').addEventListener('click', showResults)
 
 // Define getData function
-function displayData() {
+function showResults() {
+
     // Get username
     const username = document.querySelector('#username').value
-    let obj
     
-    // Fetch data
-    const url = `https://www.codewars.com/api/v1/users/${username}`
-    fetch(url)
-    .then(response => response.json())
-    .then(result => process(result))
-    .catch(error => console.error(error))
+    // Fetch data for username then render it
+    fetch(`https://www.codewars.com/api/v1/users/${username}`)
+        .then(res => res.json())
+        .then(data => showStats(data))
+        .catch(err => console.log(`Error: ${err}`))
+
 }
 
-function process(result) {
-    let title = document.querySelector('#title')
-    if (result.clan === null) {
-        title.innerHTML = `Hi, ${result.username}! You don't belong to any clan.`
-    } else {
-        title.innerHTML = `Hi, ${result.username} of ${result.clan}!`
-    }
+function showStats(result) {
 
-    let honor = document.querySelector('#honor')
-    let leaderboard = document.querySelector('#leaderboard')
-    let overallrank = document.querySelector('#overallrank')
-    let challengesCompleted = document.querySelector('#challengesCompleted')
-    let challengesAuthored = document.querySelector('#challengesAuthored')
+    // Render name
+    document.querySelector('.name > p').innerHTML = result.clan === null ? 
+        `Hi, ${result.username}! You don't belong to any clan.` : 
+        `Hi, ${result.username} of ${result.clan}!`
 
-    honor.innerHTML = result.honor
-    leaderboard.innerHTML = '#' + result.leaderboardPosition
-    overallrank.innerHTML = result.ranks.overall.name
-    challengesCompleted.innerHTML = result.codeChallenges.totalCompleted
-    challengesAuthored.innerHTML = result.codeChallenges.totalAuthored
+    // Render highlights
+    document.querySelector('#honor').innerHTML = result.honor
+    document.querySelector('#leaderboard').innerHTML = result.leaderboardPosition
+    document.querySelector('#overallrank').innerHTML = result.name
+    document.querySelector('#challengesCompleted').innerHTML = result.totalCompleted
+    document.querySelector('#challengesAuthored').innerHTML = result.totalAuthored
 
+    // Render skills
     let proglang = result.ranks.languages
     Object.keys(proglang).forEach(a => {
+        // Create skill image
         let img = document.createElement('img')
         img.src = `assets/proglang/${a}.png`
-        
+        // Create skill rank
         let kyu = document.createElement('span')
         kyu.innerHTML = 'Rank: ' + proglang[a].name
-
+        // Create skill score
         let score = document.createElement('span')
         score.innerHTML = 'Score: ' + proglang[a].score
-
+        // Render skill
         let div = document.createElement('div')
+        div.appendChild(img).appendChild(kyu).appendChild(score)
         let skills = document.querySelector('#skills')
         skills.appendChild(div)
-
-        let divLast = document.querySelector('#skills div:last-child')
-        divLast.appendChild(img)
-        divLast.appendChild(kyu)
-        divLast.appendChild(score)
     })
     
-    document.querySelector('#content').style.display = 'contents'
-    document.querySelector('#main').style.display = 'none'
+    // Hide welcome and show only contents
+    document.querySelector('#welcome').style.display = 'none'
+    document.querySelector('#results').style.display = 'contents'
+
 }
 
